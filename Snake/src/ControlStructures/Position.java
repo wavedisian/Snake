ce98feafb52d4a79960cef153d4f9a0ef900093f
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2017-12-15 09:13:47 -0500

    Psotion defined

--- Snake/src/ControlStructures/Position.java
+++ Snake/src/ControlStructures/Position.java
@@ -1,9 +1,28 @@
 package ControlStructures;
 
-public class Position {
-
-	public Position() {
-		// TODO Auto-generated constructor stub
+public class Position 
+{
+	private int x;
+	private int y;
+	private boolean f;
+	public Position(int x, int y, boolean f) 
+	{
+		this.x=x;
+		this.y=y;
+		this.f=f;
 	}
+	
+	public String printPosition()
+	{
+		if(f) 
+		{
+			return "X";
+		}
+		else
+		{
+			return "O";
+		}
+	}
+		
 
 }
