b3c670b1a8ba0592ad330749fc5e7c5f9f0bd7ec
BT_1N3_13 <BT_1N3_13@BT_1N3_13.bktech.local>
2018-01-03 09:28:27 -0500

    aweeqwe

--- Snake/src/ControlStructures/Body.java
+++ Snake/src/ControlStructures/Body.java
@@ -7,29 +7,67 @@ public class Body {
 		this.p=p;
 	}
 	
-	public void moveBody(String move)
+	public void moveBody(String move, Board g)
 	{
-		if(validMove(move))
+		if(validMove(move, g))
 		{
-			Position[] newpos = new Position[this.p.length + 1];
-			if(move.equals("a"))
+			Position[] newpos = new Position[this.p.length];
+			if(move.equals("s"))
 			{
 				newpos[0] = new Position(this.p[0].getLocation()[0], this.p[0].getLocation()[1] - 1);
-				for(int i=0; i<newpos.length; i++)
+				for(int i=0; i<newpos.length-1; i++)
 				{
 					newpos[i+1]=this.p[i];
 				}
 				this.p=newpos;
-				
+			}
+			if(move.equals("w"))
+			{
+				newpos[0] = new Position(this.p[0].getLocation()[0], this.p[0].getLocation()[1] + 1);
+				for(int i=0; i<newpos.length-1; i++)
+				{
+					newpos[i+1]=this.p[i];
+				}
+				this.p=newpos;
+			}
+			if(move.equals("a"))
+			{
+				newpos[0] = new Position(this.p[0].getLocation()[0] - 1, this.p[0].getLocation()[1]);
+				for(int i=0; i<newpos.length-1; i++)
+				{
+					newpos[i+1]=this.p[i];
+				}
+				this.p=newpos;
+			}
+			if(move.equals("d"))
+			{
+				newpos[0] = new Position(this.p[0].getLocation()[0] + 1, this.p[0].getLocation()[1] );
+				for(int i=0; i<newpos.length-1; i++)
+				{
+					newpos[i+1]=this.p[i];
+				}
+				this.p=newpos;
 			}
 		}
 	}
-	public boolean validMove(String move)
+	public boolean validMove(String move, Board g)
 	{
-		if(move.equals("w")&&(this.p[0].getLocation()[1]))
+		if(move.equals("w")&&(this.p[0].getLocation()[1]==0))
 		{
-			System.out.println();
+			return false;
 		}
-				
+		if(move.equals("s")&&(this.p[0].getLocation()[1]==g.length))
+		{
+			return false;
+		}
+		if(move.equals("a")&&(this.p[0].getLocation()[0]==0))
+		{
+			return false;
+		}
+		if(move.equals("d")&&(this.p[0].getLocation()[0]==g.length))
+		{
+			return false;
+		}
+		if(move.equals("w")
 	}
 }
---
+++ Snake/src/ControlStructures/Spawn.java
@@ -0,0 +1,13 @@
+package ControlStructures;
+
+public class Spawn extends Position {
+	private int x;
+	private int y;
+	public Spawn(int x, int y) {
+		super(x, y);
+		this.x=x;
+		this.y=y;
+	}
+	
+
+}
